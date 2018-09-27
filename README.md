4021b6ad133b268f4de0a6dc04256411d875800a
deviljin112 <39461646+deviljin112@users.noreply.github.com>
2018-09-27 12:37:32 +0100

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# ESX_THIEF
+
+Editted Version of the [esx_thief](https://github.com/ESX-PUBLIC/esx_thief) script.
+
+In this version when pressing "G" (Can be edited line 371 in Client.lua) a menu to cuff / uncuff / search / move appears. 
+
+Nothing more to it really...
+
+## TO DO
+
+    - Fix the issue with "no hands up" when searching
+    - Add only weapon when searching
+
+## BASED ON
+
+[esx_thief](https://github.com/ESX-PUBLIC/esx_thief)
+[esx_policejob](https://github.com/ESX-Org/esx_policejob)
---
+++ esx_thief/locales/en.lua
@@ -0,0 +1,22 @@
+Locales['en'] = {
+    
+    ['no_hands_up']         = 'Cel nie ma podniesionych rak!',
+    ['cash']                = 'Gotowka',
+    ['black_money']         = 'Brudna kasa',
+    ['inventory']           = 'Ekwipunek',
+    ['target_inventory']    = 'Ekwipunek celu',
+    ['steal']               = 'Rabuj',
+    ['return']              = 'Cofnij',
+    ['action_choice']       = 'Wybor akcji',
+    ['amount']              = 'Ilosc',
+    ['too_far']             = 'Jestes za daleko!',
+    ['no_players_nearby']   = 'Brak gracza w poblizu',
+    ['ex_inv_lim_source']   = 'Brak miejsca w ekwipunku',
+    ['ex_inv_lim_target']   = 'Brak miejsca w ekwipunku',
+    ['you_stole']           = 'Zrabowales',
+    ['from_your_target']    = 'od celu',
+    ['someone_stole']       = 'Ktos zrabowal',
+    ['invalid_quantity']    = 'Zla ilosc',
+
+}
+    
